0a00ca1baedee74851c5ae95e1018360280456c0
cccoolll <36098954+cccoolll@users.noreply.github.com>
2024-08-05 09:50:39 +0200

    feat: Update Hypha server URL in JavaScript client initialization for webrtc demo

--- reef_imaging/test/test-squid-hypha.py
+++ reef_imaging/test/test-squid-hypha.py
@@ -2,14 +2,15 @@ import asyncio
 from hypha_rpc import *
 
 async def main():
-    server = await connect_to_server({"server_url": "https://ai.imjoy.io"})
-
+    server = await connect_to_server({"server_url": "https://hypha.aicell.io"})
+    available_services = await server.list_services()
+    print("Available services:", available_services)
     # Get an existing service
     # Since "hello-world" is registered as a public service, we can access it using only the name "hello-world"
-    svc = await server.get_service("built-in")
-    rtc_server = await get_rtc_service(server, "squid-control-rtc")
-    print(rtc_server)
-    print("Connected to server, got service:", svc)
-    print("Connected to server, got rtc service:", rtc_server)
+    # svc = await server.get_service("built-in")
+    # rtc_server = await get_rtc_service(server, "aiortc-demo")
+    # print(rtc_server)
+    # print("Connected to server, got service:", svc)
+    # print("Connected to server, got rtc service:", rtc_server)
 if __name__ == "__main__":
     asyncio.run(main())
