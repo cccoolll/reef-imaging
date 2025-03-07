9889461071d3fad3ab38d205764817bac35c736d
cccoolll <36098954+cccoolll@users.noreply.github.com>
2025-03-07 11:22:31 +0100

    refactor: Remove unused test file for hypha service robotic arm

--- reef_imaging/control/dorna-control/start_hypha_service_robotic_arm.py
+++ reef_imaging/control/dorna-control/start_hypha_service_robotic_arm.py
@@ -12,242 +12,204 @@ ENV_FILE = dotenv.find_dotenv()
 if ENV_FILE:  
     dotenv.load_dotenv(ENV_FILE)  
 
-class DornaController:
-    def __init__(self, ip="192.168.2.20"):
+class RoboticArmService:
+    def __init__(self, local):
+        self.local = local
+        self.server_url = "http://localhost:9527" if local else "https://hypha.aicell.io"
         self.robot = Dorna()
-        self.ip = ip
+        self.ip = "192.168.2.20"
         self.connected = False
 
-    async def connect(self):
-        await asyncio.to_thread(self.robot.connect, self.ip)
-        self.connected = True
-        print("Connected to robot")
-        return f"Connected to robot"
+    async def start_hypha_service(self, server):
+        svc = await server.register_service({
+            "name": "Robotic Arm Control",
+            "id": "robotic-arm-control",
+            "config": {
+                "visibility": "public",
+                "run_in_executor": True
+            },
+            "move_sample_from_microscope1_to_incubator": self.move_sample_from_microscope1_to_incubator,
+            "move_sample_from_incubator_to_microscope1": self.move_sample_from_incubator_to_microscope1,
+            "grab_sample_from_microscope1": self.grab_sample_from_microscope1,
+            "grab_sample_from_incubator": self.grab_sample_from_incubator,
+            "put_sample_on_microscope1": self.put_sample_on_microscope1,
+            "put_sample_on_incubator": self.put_sample_on_incubator,
+            "transport_from_incubator_to_microscope1": self.transport_from_incubator_to_microscope1,
+            "transport_from_microscope1_to_incubator": self.transport_from_microscope1_to_incubator,
+            "connect": self.connect,
+            "disconnect": self.disconnect,
+            "halt": self.halt,
+        })
 
-    async def disconnect(self):
-        await asyncio.to_thread(self.robot.close)
-        self.connected = False
-        print("Disconnected from robot")
-        return f"Disconnected from robot"
-    
-    async def set_motor(self, state: int=Field(1, description="Enable or disable the motor, 1 for enable, 0 for disable")):
-        if not self.connected:
-            await self.connect()
-        await asyncio.to_thread(self.robot.set_motor, state)
-        return f"Motor set to {state}"
+        print(f"Robotic arm control service registered at workspace: {server.config.workspace}, id: {svc.id}")
+        print(f'You can use this service using the service id: {svc.id}')
+        id = svc.id.split(":")[1]
+        print(f"You can also test the service via the HTTP proxy: {self.server_url}/{server.config.workspace}/services/{id}/move_sample_from_microscope1_to_incubator")
 
-    async def play_script(self, script_path):
-        if not self.connected:
-            await self.connect()
-        await asyncio.to_thread(self.robot.play_script, script_path)
-        return "Script played"
-    
-    async def move_sample_from_microscope1_to_incubator(self):
-        await self.set_motor(1)
-        await self.play_script("paths/microscope1_to_incubator.txt")
-        return "Sample moved from microscope1 to incubator"
-    
-    async def grab_sample_from_microscope1(self):
-        await self.set_motor(1)
-        await self.play_script("paths/grab_from_microscope1.txt")
-        return "Sample grabbed from microscope1"
+    async def setup(self):
+        if self.local:
+            token = None
+            server = await connect_to_server({"server_url": self.server_url, "token": token, "ping_interval": None})
+        else:
+            try:
+                token = os.environ.get("REEF_WORKSPACE_TOKEN")
+            except:
+                token = await login({"server_url": self.server_url})
+            server = await connect_to_server({"server_url": self.server_url, "token": token, "workspace": "reef-imaging", "ping_interval": None})
 
-    async def grab_sample_from_incubator(self):
-        await self.set_motor(1)
-        await self.play_script("paths/grab_from_incubator.txt")
-        return "Sample grabbed from incubator"
-    
-    async def put_sample_on_microscope1(self):
-        await self.set_motor(1)
-        await self.play_script("paths/put_on_microscope1.txt")
-        return "Sample placed on microscope1"
+        await self.start_hypha_service(server)
 
-    async def put_sample_on_incubator(self):
-        await self.set_motor(1)
-        await self.play_script("paths/put_on_incubator.txt")
-        return "Sample placed on incubator"
-    
-    async def transport_from_incubator_to_microscope1(self):
-        await self.set_motor(1)
-        await self.play_script("paths/transport_from_incubator_to_microscope1.txt")
-        return "Sample moved from incubator to microscope1"
-    
-    async def transport_from_microscope1_to_incubator(self):
-        await self.set_motor(1)
-        await self.play_script("paths/transport_from_microscope1_to_incubator.txt")
-        return "Sample moved from microscope1 to incubator"
-
-    async def move_sample_from_incubator_to_microscope1(self):
-        await self.set_motor(1)
-        await self.play_script("paths/incubator_to_microscope1.txt")
-        return "Sample moved from incubator to microscope1"
-
-    
-    async def halt(self):
-        if not self.connected:
-            await self.connect()
-        await asyncio.to_thread(self.robot.halt)
-        print("Robot halted")
-
-robotic_arm = DornaController()
-
-async def start_server(server_url, local):
-    if local:
-        token = None
-        server = await connect_to_server({"server_url": server_url, "token": token, "ping_interval": None})
-    else:
-        try:
-            token = os.environ.get("REEF_WORKSPACE_TOKEN")
-        except:
-            token = await login({"server_url": server_url})
-        server = await connect_to_server({"server_url": server_url, "token": token, "workspace": "reef-imaging", "ping_interval": None})
-
-    @schema_function()
-    async def move_sample_from_microscope1_to_incubator():
-        """
-        Move sample from microscope1 to incubator, the microscope need to be homed before
-        Returns: bool True
-        """
-        await robotic_arm.move_sample_from_microscope1_to_incubator()
-        print("Sample moved from microscope1 to incubator")
-        return True
-    
-    @schema_function()
-    async def move_sample_from_incubator_to_microscope1():
-        """
-        Move sample from incubator to microscope1, microscope need to be homed before
-        Returns: bool True
-        """
-        await robotic_arm.move_sample_from_incubator_to_microscope1()
-        print("Sample moved from incubator to microscope1")
-        return True
-    
-    @schema_function()
-    async def grab_sample_from_microscope1():
-        """
-        Description:
-        Transport a sample from microscope1 to the incubator
-        Returns: bool True
-        """
-        await robotic_arm.grab_sample_from_microscope1()
-        print("Sample grabbed from microscope1")
-        return True
-    
-    @schema_function()
-    async def grab_sample_from_incubator():
-        """
-        Grab a sample from the incubator
-        Returns: bool True
-        """
-        await robotic_arm.grab_sample_from_incubator()
-        print("Sample grabbed from incubator")
-        return True
-    
-    @schema_function()
-    async def put_sample_on_microscope1():
-        """
-        Place a sample on microscope1
-        Returns:bool True
-        """
-        await robotic_arm.put_sample_on_microscope1()
-        print("Sample placed on microscope1")
-        return True
-        
-    @schema_function()
-    async def put_sample_on_incubator():
-        """
-        Place a sample on the incubator.
-        Returns: bool True
-        """
-        await robotic_arm.put_sample_on_incubator()
-        print("Sample placed on incubator")
-        return True
-
-    @schema_function()
-    async def transport_from_incubator_to_microscope1():
-        """
-        Transport a sample from the incubator to microscope1
-        Returns: bool True
-        """
-        await robotic_arm.transport_from_incubator_to_microscope1()
-        print("Sample moved from incubator to microscope1")
-        return True
-    
-    @schema_function()
-    async def transport_from_microscope1_to_incubator():
-        """
-        Transport a sample from microscope1 to the incubator
-        Returns: bool True
-        """
-        await robotic_arm.transport_from_microscope1_to_incubator()
-        print("Sample moved from microscope1 to incubator")
-        return True
-
-    @schema_function()
-    async def connect():
+    @schema_function(skip_self=True)
+    def connect(self):
         """
         Connect and occupy the robot, so that it can be controlled.
         Returns: bool True
         """
-        await robotic_arm.connect()
-        print("Connected to robotic arm")
+        self.robot.connect(self.ip)
+        self.connected = True
+        print("Connected to robot")
         return True
 
-    @schema_function()
-    async def disconnect():
+    @schema_function(skip_self=True)
+    def disconnect(self):
         """
         Disconnect the robot, so that it can be used by other clients.
         Returns: bool True
         """
-        await robotic_arm.disconnect()
-        print("Disconnected from robotic arm")
+        self.robot.close()
+        self.connected = False
+        print("Disconnected from robot")
         return True
-    
-    @schema_function()
-    async def halt():
+
+    @schema_function(skip_self=True)
+    def set_motor(self, state: int=Field(1, description="Enable or disable the motor, 1 for enable, 0 for disable")):
+        if not self.connected:
+            self.connect()
+        self.robot.set_motor(state)
+        return f"Motor set to {state}"
+
+    @schema_function(skip_self=True)
+    def play_script(self, script_path):
+        if not self.connected:
+            self.connect()
+        self.robot.play_script(script_path)
+        return "Script played"
+
+    @schema_function(skip_self=True)
+    def move_sample_from_microscope1_to_incubator(self):
         """
-        Description:
+        Move sample from microscope1 to incubator, the microscope need to be homed before
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/microscope1_to_incubator.txt")
+        print("Sample moved from microscope1 to incubator")
+        return True
+
+    @schema_function(skip_self=True)
+    def move_sample_from_incubator_to_microscope1(self):
+        """
+        Move sample from incubator to microscope1, microscope need to be homed before
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/incubator_to_microscope1.txt")
+        print("Sample moved from incubator to microscope1")
+        return True
+
+    @schema_function(skip_self=True)
+    def grab_sample_from_microscope1(self):
+        """
+        Transport a sample from microscope1 to the incubator
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/grab_from_microscope1.txt")
+        print("Sample grabbed from microscope1")
+        return True
+
+    @schema_function(skip_self=True)
+    def grab_sample_from_incubator(self):
+        """
+        Grab a sample from the incubator
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/grab_from_incubator.txt")
+        print("Sample grabbed from incubator")
+        return True
+
+    @schema_function(skip_self=True)
+    def put_sample_on_microscope1(self):
+        """
+        Place a sample on microscope1
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/put_on_microscope1.txt")
+        print("Sample placed on microscope1")
+        return True
+
+    @schema_function(skip_self=True)
+    def put_sample_on_incubator(self):
+        """
+        Place a sample on the incubator.
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/put_on_incubator.txt")
+        print("Sample placed on incubator")
+        return True
+
+    @schema_function(skip_self=True)
+    def transport_from_incubator_to_microscope1(self):
+        """
+        Transport a sample from the incubator to microscope1
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/transport_from_incubator_to_microscope1.txt")
+        print("Sample moved from incubator to microscope1")
+        return True
+
+    @schema_function(skip_self=True)
+    def transport_from_microscope1_to_incubator(self):
+        """
+        Transport a sample from microscope1 to the incubator
+        Returns: bool True
+        """
+        self.set_motor(1)
+        self.play_script("paths/transport_from_microscope1_to_incubator.txt")
+        print("Sample moved from microscope1 to incubator")
+        return True
+
+    @schema_function(skip_self=True)
+    def halt(self):
+        """
         Halt/stop the robot, stop all the movements
         Returns: bool True
         """
-        print("Halting robotic arm")
-        await robotic_arm.halt()
+        if not self.connected:
+            self.connect()
+        self.robot.halt()
+        print("Robot halted")
         return True
-    
-    await robotic_arm.connect()
-
-    svc = await server.register_service({
-        "name": "Robotic Arm Control",
-        "id": "robotic-arm-control",
-        "config": {
-            "visibility": "public",
-        },
-        "move_sample_from_microscope1_to_incubator": move_sample_from_microscope1_to_incubator,
-        "move_sample_from_incubator_to_microscope1": move_sample_from_incubator_to_microscope1,
-        "grab_sample_from_microscope1": grab_sample_from_microscope1,
-        "grab_sample_from_incubator": grab_sample_from_incubator,
-        "put_sample_on_microscope1": put_sample_on_microscope1,
-        "put_sample_on_incubator": put_sample_on_incubator,
-        "transport_from_incubator_to_microscope1": transport_from_incubator_to_microscope1,
-        "transport_from_microscope1_to_incubator": transport_from_microscope1_to_incubator,
-        "connect": connect,
-        "disconnect": disconnect,
-        "halt": halt,
-    })
-
-    print(f"Incubator control service registered at workspace: {server.config.workspace}, id: {svc.id}")
-
-    print(f'You can use this service using the service id: {svc.id}')
-    id = svc.id.split(":")[1]
-    print(f'You can use this service using the service id: {id}')
-    # Keep the server running
-    await server.serve()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Start the Hypha service for the robotic arm.")
     parser.add_argument('--local', action='store_true', help="Use localhost as server URL")
     args = parser.parse_args()
 
-    server_url = "http://localhost:9527" if args.local else "https://hypha.aicell.io"
+    robotic_arm_service = RoboticArmService(local=args.local)
 
-    asyncio.run(start_server(server_url, args.local))
+    loop = asyncio.get_event_loop()
+
+    async def main():
+        try:
+            await robotic_arm_service.setup()
+        except Exception as e:
+            print(f"Error setting up robotic arm service: {e}")
+            raise e
+
+    loop.create_task(main())
+    loop.run_forever()
